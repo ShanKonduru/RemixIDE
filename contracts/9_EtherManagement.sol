43abb79d4afaae81ed65a5eed0c8a48861984aa4
shankonduru <shankonduru@gmail.com>
2023-01-10 20:45:01 +0530

    added example for Eth wallet Management

---
+++ contracts/9_EtherManagement.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.7;
+
+contract EtherManagement {
+    mapping (address => uint256) public balances;
+    address payable wallet; 
+
+    constructor(address payable _wallet) {
+        wallet = _wallet;
+    }
+
+    function buyToken() public payable  {
+        // Buy a Token 
+        balances[msg.sender] += 1;
+        // Send Eth to a Wallet
+        wallet.transfer(msg.value);
+    }
+}
